560d4cbdbf0abd99adb70fa6900a4e1dab6023eb
BrettREvans <Brett704@revature.net>
2022-04-08 15:36:11 -0700

    Backend Skeleton created

---
+++ src/test/java/com/revature/williams_kitchen/WilliamsKitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.williams_kitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WilliamsKitchenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
