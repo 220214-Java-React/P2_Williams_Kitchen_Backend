6e6f9f42df2f0cbe835fbea067064da51e2755f6
William Gaines <willny24@gmail.com>
2022-04-09 11:44:00 -0400

    Initial Commit (User model and relevant classes)
    Created general API skeleton with controller, model, repository, and service with matching classes for handling users

---
+++ src/main/java/com/revature/williams_kitchen/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.revature.williams_kitchen.controller;
+
+
+import com.revature.williams_kitchen.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController
+{
+    @Autowired
+    UserService userService;
+
+
+
+}
