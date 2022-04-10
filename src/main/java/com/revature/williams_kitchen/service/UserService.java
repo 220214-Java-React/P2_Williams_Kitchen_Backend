8fe128cf4895f721ff05fc02367674b9ccbd280f
BrettREvans <Brett704@revature.net>
2022-04-09 20:57:28 -0700

    User Controller and User Service Handles creating a new user and persisting it to a database

--- src/main/java/com/revature/williams_kitchen/service/UserService.java
+++ src/main/java/com/revature/williams_kitchen/service/UserService.java
@@ -3,11 +3,15 @@ package com.revature.williams_kitchen.service;
 import com.revature.williams_kitchen.model.User;
 import com.revature.williams_kitchen.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import java.util.List;
-
+@Service
 public class UserService
 {
     @Autowired
     UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
 }
